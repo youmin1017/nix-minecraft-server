71595e4d83800e2954bda767512d3722df8d0133
youmin1017 <youmin.main@gmail.com>
2025-06-25 16:57:04 +0800

    fix: bootloader

--- hosts/nixos-mc/configuration.nix
+++ hosts/nixos-mc/configuration.nix
@@ -27,8 +27,11 @@
   ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
 
   nix.settings.auto-optimise-store = true;
   nix.settings.experimental-features = [
